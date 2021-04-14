76d88ad40fd8bdbc25dedb2908bc6b1f35353fb5
AnwarKamel <anwarinfo24@gmail.com>
2021-04-14 12:20:13 +0100

    Initial commit

---
+++ FireBaseSenAndReadDataESP8266.ino
@@ -0,0 +1,98 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// config
+#define FIREBASE_HOST "Your Firebase Host"
+#define FIREBASE_AUTH "Your Firebase Key"
+#define WIFI_SSID "Your WIFI SSID"
+#define WIFI_PASSWORD "Your WIFI PASSWORD"
+
+void setup() {
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.setString("username","");
+}
+
+int n = 0;
+
+void loop() {
+  // set value
+  Firebase.setFloat("number", 1);
+  Firebase.setString("name","Anwar");
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  
+  // update value
+  Firebase.setFloat("number", 2);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+
+  // get value 
+  Serial.print("number: ");
+  Serial.println(Firebase.getFloat("number"));
+  delay(1000);
+
+  // get value 
+  Serial.print("username: ");
+  Serial.println(Firebase.getString("username"));
+  delay(1000);
+
+  
+  // remove value
+  Firebase.remove("number");
+  delay(1000);
+
+  // set string value
+  Firebase.setString("message", "hello Anwar");
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /message failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  
+  // set bool value
+  Firebase.setBool("truth", false);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /truth failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+
+  // append a new value to /logs
+  String name = Firebase.pushInt("logs", n++);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("pushing /logs failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  Serial.print("pushed: /logs/");
+  Serial.println(name);
+  delay(1000);
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Send And Read Data Firebase And NodeMcu
+ Simple example Using NodeMcu Send And Read DATA With Firebase
+
+
+### Config : 
+#### 1- edit 
+
+```C++
+// Set these to run example.
+#define FIREBASE_HOST "Your Firebase Host"
+#define FIREBASE_AUTH "Your Firebase Key"
+#define WIFI_SSID "Your WIFI SSID"
+#define WIFI_PASSWORD "Your WIFI PASSWORD"
+
+```
+
+## result :
+![Screen](images/img1.gif)
+
+![screnn2](images/img2.PNG)
